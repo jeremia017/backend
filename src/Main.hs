e86ecb7dc8f6bb8d99f40266b17dbf2a0c2784e5
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-11-22 22:30:11 +0700

    feat: move main inside src

---
+++ src/Main.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Monoid (mconcat)
+import Web.Scotty
+
+main = scotty 3000 $
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
