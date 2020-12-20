1c761ae28161f19bd26bbcdeb2df89184ef4b8fc
Jeremia Delviero <jeremiadelviero@gmail.com>
2020-12-20 17:32:12 +0700

    modify scotty main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
-
-import Data.Monoid (mconcat)
 import Web.Scotty
+import Network.HTTP.Types
 
-main = scotty 3000 $
-  get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+main = scotty 3000 $ do
+  get "/" $ do                         -- handle GET request on "/" URL
+    text "This was a GET request!"     -- send 'text/plain' response
+  delete "/" $ do
+    html "This was a DELETE request!"  -- send 'text/html' response
+  post "/" $ do
+    text "This was a POST request!"
+  put "/" $ do
+    text "This was a PUT request!"
