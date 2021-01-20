66c7abeeb0489419e36f3a1e239eec13ec9ed5e5
Jeremia Delviero <jeremiadelviero@gmail.com>
2021-01-20 12:43:06 +0700

    add recommendation feature

---
+++ src/Handler/Recommendation.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Recommendation where
+
+import Import
+
+getRecommendation1R :: RecommendationId -> Handler Value
+getRecommendation1R recommendationId = do
+    recommendation <- runDB $ get404 recommendationId
+
+    return $ object ["recommendation" .= (Entity recommendationId recommendation)]
+
+postRecommendation2R :: Handler Value
+postRecommendation2R = do
+    recommendation <- requireCheckJsonBody :: Handler Recommendation
+    recommendationId <- runDB $ insert recommendation
+
+    sendStatusJSON ok200 (object ["recommendationId" .= recommendationId])
+
+getRecommendationsR :: Handler Value
+getRecommendationsR = do
+    recommendations <- runDB $ selectList [] [Desc RecommendationId]
+    sendStatusJSON ok200 (object ["recommendations" .= recommendations])
